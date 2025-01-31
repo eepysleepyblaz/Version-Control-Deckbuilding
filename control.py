f5e33e31298c9a333ad1788c16aa0ce03048137a
Sandy Emmerson <2893523e@student.gla.ac.uk>
2025-01-31 00:31:09 +0000

    Added basic deck display functionality

--- control.py
+++ control.py
@@ -6,6 +6,8 @@ import os
 #For maintaining file upload times
 import datetime
 
+#For get card images
+import webbrowser
 
 #Handles the creation of vertions of file
 class Version_Control():
@@ -33,8 +35,52 @@ class Version_Control():
             f.write(content)
 
     #Lists all the versions associated with this file
+    def list(self):
+        files = os.listdir(self.full_path)
+        return files
+    
+    #Creates a webpage of a specific deck list and opens it
+    def webshow(self, filename):
+        with open(os.path.join(self.full_path, filename)) as f:
+            contents = f.read()
+        
+        contents = contents.split("\n")
+
+        #Temporaily creates an html file to view the page with
+        with open('temp.htm', 'w') as f:
+            f.write("<!DOCTYPE html>\n")
+            f.write("<html>\n")
+            f.write("<head>\n")
+            f.write(f"<title>{self.name}</title>\n")
+            f.write("</head>\n")
+            f.write("<body>\n")
+
+            #Loops to add all the card images
+            for card in contents:
+                if card != "":
+                    amount = int(card.split(" ")[0])
+                    cardname = card.split(" ")[1:]
+                    cardname_formatted = "+".join(cardname)
+                    for i in range(amount):
+                        f.write(f'<img src="https://api.scryfall.com/cards/named?exact={cardname_formatted}&format=image" width="252" height="352">\n')
+
+            f.write("</body>\n")
+            f.write("</html>\n")
+        
+        #Opens the file
+        webbrowser.open('temp.htm')
+        input()
+        os.remove('temp.htm')
 
 
 
+
+
+
+
+
+#Test code
 a = Version_Control("test")
-a.commit("test.txt")
+a.commit("C:/Users/myaccount/Downloads/mono-blue.txt")
+print(a.list())
+a.webshow("test-[30-1-2025][23-38].txt")
